819654c360763b61b054cb3c7943108d1aa22de4
NikoPikka <n7pini00@students.oamk.fi>
2019-03-19 11:43:39 +0200

    league and team pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },  { path: 'leagues', loadChildren: './leagues/leagues.module#LeaguesPageModule' },
+  { path: 'league', loadChildren: './league/league.module#LeaguePageModule' },
+  { path: 'team', loadChildren: './team/team.module#TeamPageModule' },
 
 ];
 
