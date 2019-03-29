9a247bd0edd1da4ee46ac8cde6df0daf8bef5695
NikoPikka <n7pini00@students.oamk.fi>
2019-03-29 11:58:33 +0200

    Planning the schedule tab

--- src/app/schedule-tab/schedule-tab.page.ts
+++ src/app/schedule-tab/schedule-tab.page.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule-tab.page.scss'],
 })
 export class ScheduleTabPage implements OnInit {
-
-  constructor() { }
+  team1s = [];
+  team2s = [];
+  scores = [];
+  constructor() {}
 
   ngOnInit() {
-  }
+    this.team1s = [
+      "Everton",
+      "Liverpool",
+      "Manchester United"
+    ];
 
+    this.team2s = [
+      "Burnley",
+      "Brighton",
+      "Arsenal"
+    ];
+
+    this.scores = [
+      "0 - 0",
+      "1 - 1",
+      "2 - 2"
+    ];
+  }
+    
 }
