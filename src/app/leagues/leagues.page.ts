8b7b8540f129e0176e71a1ab69ad2bcb1226564c
Aleksi Jutila <n6jual00@students.oamk.fi>
2019-04-08 12:44:55 +0300

    Leagues.page data fetch

--- src/app/leagues/leagues.page.ts
+++ src/app/leagues/leagues.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FootballService } from '../football.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-leagues',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leagues.page.scss'],
 })
 export class LeaguesPage implements OnInit {
+leagues: any;
+id: any;
 
-  constructor() { }
+  constructor(private footballService: FootballService, private router: Router) { }
 
   ngOnInit() {
+    this.footballService
+    .getData('/competitions/?plan=TIER_ONE')
+    .subscribe(data => {
+      this.leagues = data;
+      console.log(this.leagues);
+      for(let competitions of this.leagues.competitions)  {
+          console.log(competitions.id);
+      }
+      })
   }
-
 }
+
