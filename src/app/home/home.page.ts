ae2fa09cab3f3448ceea61853297d8574d64b8bf
sanosena <48476515+sanosena@users.noreply.github.com>
2019-04-10 21:37:00 +0300

    homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,19 +10,54 @@ import { Router } from '@angular/router';
 })
 export class HomePage implements OnInit {
   
+  allMatches: any;
   matches: any[];
-  test: string = '204950';
+  matches2: any[];
+  upcomingOpen: boolean = false;
+  scoresOpen: boolean = false;
+
+  top1: boolean = true;
+  top2: boolean = false;
+  //top3: boolean = false;
+
 
   constructor( private footballService: FootballService, private router: Router) {
   }
 
   ngOnInit() {
+    this.premierLeague();
+  }
+
+  premierLeague() {
+    this.top1 = true;
+    this.top2 = false;
+    if(!this.matches) {this.getData(2021);}
+  }
+
+  serieA() {
+    this.top1 = false;
+    this.top2 = true;
+    if(!this.matches2) {this.getData(2013);}
+  }
+
+  getData(id) {
     this.footballService
-    .getData('/competitions/2021/matches')
+    .getData('/competitions/' + id + '/matches')
     .subscribe(data => {
-      this.matches = data;
-      console.log(this.matches);
+      this.allMatches = data as any[];
+      if(id=2021) {this.matches= this.allMatches.matches;}
+      if(id=2013) {this.matches2= this.allMatches.matches;}          
     })
+  }
+
+
+
+  upcoming() {
+    this.upcomingOpen = (this.upcomingOpen === true) ? false: true;
+  }
+
+  scores() {
+    this.scoresOpen = (this.scoresOpen === true) ? false: true;
   }
 
 }
