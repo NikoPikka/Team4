361505297dc2c16e7645a3a922c0d89c1500b673
sanosena <48476515+sanosena@users.noreply.github.com>
2019-04-11 11:27:28 +0300

    homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,6 +15,14 @@ export class HomePage implements OnInit {
   matches2: any[];
   matches3: any[];
 
+  league1: string = 'Premier League';
+  league2: string = 'Serie A';
+  league3: string = 'Primera Division';
+
+  leagueId1: number = 2021; // Premier League
+  leagueId2: number = 2019; // Serie A
+  leagueId3: number = 2014; // LA League / Primera Division
+
   upcomingOpen: boolean = false;
   scoresOpen: boolean = false;
 
@@ -23,43 +31,41 @@ export class HomePage implements OnInit {
   top3: boolean = false;
 
 
-
-
   constructor( private footballService: FootballService, private router: Router) {
   }
 
   ngOnInit() {
-    this.premierLeague();
+    this.topLeague1();
   }
 
-  premierLeague() {
+  topLeague1() {
     this.upcomingOpen = false;
     this.scoresOpen = false;
     this.top1 = true;
     this.top2 = false;
     this.top3 = false;
 
-    if (!this.matches1) {this.getData(2021)};
+    if (!this.matches1) {this.getData(this.leagueId1)};
   }
 
-  serieA() {
+  topLeague2() {
     this.upcomingOpen = false;
     this.scoresOpen = false;
     this.top1 = false;
     this.top2 = true;
     this.top3 = false;
 
-    if (!this.matches2) {this.getData(2013)};
+    if (!this.matches2) {this.getData(this.leagueId2)};
   }
 
-  laLeague() {
+  topLeague3() {
     this.upcomingOpen = false;
     this.scoresOpen = false;
     this.top1 = false;
     this.top2 = false;
     this.top3 = true;
 
-    if (!this.matches3) {this.getData(2014)};
+    if (!this.matches3) {this.getData(this.leagueId3)};
   }
 
   getData(id) {
@@ -67,12 +73,26 @@ export class HomePage implements OnInit {
     .getData('/competitions/' + id + '/matches')
     .subscribe(data => {
       this.allMatches = data as any[];
-      if(id===2021) {this.matches1= this.allMatches.matches; console.log(this.matches1)}
-      if(id===2013) {this.matches2= this.allMatches.matches; console.log(this.matches2)}
-      if(id===2014) {this.matches3= this.allMatches.matches; console.log(this.matches3)}           
+      //console.log(this.allMatches)
+
+      if(id===this.leagueId1) {
+        this.matches1= this.allMatches.matches;
+        this.league1= this.allMatches.competition.name;
+        console.log(this.matches1)
+      }
+      if(id===this.leagueId2) {
+        this.matches2= this.allMatches.matches;
+        this.league2= this.allMatches.competition.name;
+        console.log(this.matches2)
+      }
+      if(id===this.leagueId3) {
+        this.matches3= this.allMatches.matches;
+        this.league3= this.allMatches.competition.name;
+        console.log(this.matches3)
+      }  
+
     })
   }
-
 
 
   upcoming() {
