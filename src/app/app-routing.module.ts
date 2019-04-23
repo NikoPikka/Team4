faf9ed8e20520f0b0799abc978a58e7e135a2542
sanosena <48476515+sanosena@users.noreply.github.com>
2019-04-23 11:24:06 +0300

    League title, league name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'leagues', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'leagues', loadChildren: './leagues/leagues.module#LeaguesPageModule' },
-  { path: 'league/:id', loadChildren: './league/league.module#LeaguePageModule' },
+  { path: 'league/:id/:name', loadChildren: './league/league.module#LeaguePageModule' },
   { path: 'team/:id', loadChildren: './team/team.module#TeamPageModule' }
   
 ];
